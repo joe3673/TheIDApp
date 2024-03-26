acee275e4229b92d3db5bb91533900a40ccf6e34
joseph <joseph.baker@mykenzie.snhu.edu>
2024-03-25 18:19:25 -0600

    Fixed spring annotations.

--- Application/src/main/java/com/Techmotion/appserver/service/UserService.java
+++ Application/src/main/java/com/Techmotion/appserver/service/UserService.java
@@ -1,4 +1,23 @@
 package com.Techmotion.appserver.service;
 
+import com.Techmotion.appserver.service.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+
+    private final User user;
+
+
+    @Autowired
+    public UserService(User user) {
+        this.user = user;
+    }
+
+    public User getUser(){
+
+
+    }
 }
--- Application/src/main/java/com/Techmotion/appserver/service/model/User.java
+++ Application/src/main/java/com/Techmotion/appserver/service/model/User.java
@@ -50,21 +50,6 @@ public class User {
     private SubscriptionInfo subscriptionInfo;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public User(String userId, String userName, String password, String email, String firstName, String lastName, int age) {
         this.userId = userId;
         this.userName = userName;
@@ -73,5 +58,105 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public AccountStatus getAccountStatus() {
+        return accountStatus;
+    }
+
+    public void setAccountStatus(AccountStatus accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    public BusinessProfile getBusinessProfile() {
+        return businessProfile;
+    }
+
+    public void setBusinessProfile(BusinessProfile businessProfile) {
+        this.businessProfile = businessProfile;
+    }
+
+    public ContactInformation getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(ContactInformation contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+
+    public DatingProfile getDatingProfile() {
+        return datingProfile;
+    }
+
+    public void setDatingProfile(DatingProfile datingProfile) {
+        this.datingProfile = datingProfile;
+    }
+
+    public PersonalProfile getPersonalProfile() {
+        return personalProfile;
+    }
+
+    public void setPersonalProfile(PersonalProfile personalProfile) {
+        this.personalProfile = personalProfile;
+    }
+
+    public Preferences getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(Preferences preferences) {
+        this.preferences = preferences;
+    }
+
+    public PrivacySettings getPrivacySettings() {
+        return privacySettings;
+    }
+
+    public void setPrivacySettings(PrivacySettings privacySettings) {
+        this.privacySettings = privacySettings;
+    }
+
+    public Security getSecurity() {
+        return security;
+    }
+
+    public void setSecurity(Security security) {
+        this.security = security;
+    }
+
+    public SubscriptionInfo getSubscriptionInfo() {
+        return subscriptionInfo;
+    }
+
+    public void setSubscriptionInfo(SubscriptionInfo subscriptionInfo) {
+        this.subscriptionInfo = subscriptionInfo;
     }
 }
